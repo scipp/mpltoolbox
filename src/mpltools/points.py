68d26d508cbe78ddafc9c9c4526064e7676d571e
Neil Vaytet <neil.vaytet@esss.se>
2022-07-07 23:07:47 +0200

    draggable dots

--- src/mpltools/points.py
+++ src/mpltools/points.py
@@ -15,14 +15,18 @@ class Points:
         self._connections['pick_event'] = self._fig.canvas.mpl_connect(
             'pick_event', self._on_pick)
 
+        self.on_motion_notify = None
         self.on_button_press = None
         self.on_pick = None
+
+        self._pick_lock = False
+        self._moving_dot_indices = None
 
     def _make_scatter(self, x=0, y=0):
         self._scatter = self._ax.scatter([x], [y], picker=True)
 
     def _on_button_press(self, event):
-        if event.button != 1:
+        if event.button != 1 or self._pick_lock:
             return
         x, y = event.xdata, event.ydata
         if None in (x, y):
@@ -44,7 +48,36 @@ class Points:
         self._scatter.set_offsets(offsets)
 
     def _on_pick(self, event):
-        if event.mouseevent.button == 3:
+        button = event.mouseevent.button
+        if button == 1:
+            self._pick_lock = True
+            self._activate_moving_dot(event)
+        if button == 3:
             self._remove_point(event.ind)
         if self.on_pick is not None:
             self.on_pick(event)
+
+    def _activate_moving_dot(self, event):
+        self._connections['motion_notify_event'] = self._fig.canvas.mpl_connect(
+            'motion_notify_event', self._on_motion_notify)
+        self._connections['button_release_event'] = self._fig.canvas.mpl_connect(
+            'button_release_event', self._on_button_release)
+        self._moving_dot_indices = event.ind
+
+    def _on_motion_notify(self, event):
+        self._move_dot(event)
+        if self.on_motion_notify is not None:
+            self.on_motion_notify(event)
+
+    def _move_dot(self, event):
+        if None in (event.xdata, event.ydata):
+            return
+        ind = self._moving_dot_indices[0]
+        offsets = self._scatter.get_offsets()
+        offsets[ind] = [event.xdata, event.ydata]
+        self._scatter.set_offsets(offsets)
+
+    def _on_button_release(self, event):
+        self._fig.canvas.mpl_disconnect(self._connections['motion_notify_event'])
+        self._fig.canvas.mpl_disconnect(self._connections['button_release_event'])
+        self._pick_lock = False
